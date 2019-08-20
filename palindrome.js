aec335d36a85f0fa19e6c2d516e1ab6bb1b928a9
ThiagoBussola <37162902+ThiagoBussola@users.noreply.github.com>
2019-08-20 08:08:55 -0300

    Update palindrome.js
    translate palavra to word

--- palindrome.js
+++ palindrome.js
@@ -1,19 +1,19 @@
 //recebe a palavra passada na função isPalindrome para reverter ela
-function reverseWord(palavra){
-    palavra = palavra.split(""); //convert 'palavra' para array
-    palavra = palavra.reverse(); //reverse 'palavra' order
-    palavra = palavra.join(""); // do array ele monta a string novamente
-    return palavra;
+function reverseWord(word){
+    word = word.split(""); //convert 'word' to array
+    word = word.reverse(); //reverse 'word' order
+    word = word.join(""); // do array ele monta a string novamente
+    return word;
 }
 
-function isPalindrome(palavra){
+function isPalindrome(word){
     //chama a função passando como parametro para a outra função a palavra que já foi passada como parametro para essa função
-    if(palavra == reverseWord(palavra)){
-        console.log(palavra);
-        console.log("É palindromo");
+    if(word == reverseWord(word)){
+        console.log(word);
+        console.log("Is Palindrome");
     } else {
-        console.log(palavra);
-        console.log("Não é palindromo");
+        console.log(word);
+        console.log("It's not a Palindrome");
     }
     
 }
