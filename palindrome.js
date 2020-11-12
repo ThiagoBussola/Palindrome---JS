b43aedb20b389eaebce53046be206c56d8e8c572
ThiagoBussola <37162902+ThiagoBussola@users.noreply.github.com>
2020-11-12 13:03:31 +0000

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,13 +1,10 @@
-//recebe a palavra passada na função isPalindrome para reverter ela
+
 function reverseWord(word){
-    word = word.split(""); //convert 'word' to array
-    word = word.reverse(); //reverse 'word' order
-    word = word.join(""); // do array ele monta a string novamente
+    word = word.split("").reverse().join("");
     return word;
 }
 
 function isPalindrome(word){
-    //chama a função passando como parametro para a outra função a palavra que já foi passada como parametro para essa função
     if(word == reverseWord(word)){
         console.log(word);
         console.log("Is Palindrome");
